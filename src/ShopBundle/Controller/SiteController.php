df867bce33ea9b6b086874e0f8c3b19bd28b4717
edik <edik@example.ru>
2016-05-03 17:16:09 +0300

    first commit

--- src/ShopBundle/Controller/SiteController.php
+++ src/ShopBundle/Controller/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace ShopBundle\Controller;
 
+use ShopBundle\Entity\Products;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,6 +10,28 @@ class SiteController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
         return $this->render("ShopBundle:Site:index.html.twig");
+    }
+
+    public function createAction()
+    {
+        echo "EDDDDDJ";
+        exit;
+        $product = new Products();
+        $product->setName('KIWI');
+        $product->setPrice(19.99);
+        $product->setCount(2);
+
+        $em = $this->getDoctrine()->getManager();
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $em->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();
+
+        /*return new Response('Saved new product with id '.$product->getId());*/
+        return $this->render("ShopBundle:Site:create.html.twig");
     }
 }
---
+++ src/ShopBundle/Entity/Products.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Products
+ *
+ * @ORM\Table(name="products")
+ * @ORM\Entity
+ */
+class Products
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=80, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $price;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="count", type="integer", nullable=true)
+     */
+    private $count;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="feedback_id", type="integer", nullable=true)
+     */
+    private $feedbackId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Products
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return Products
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set count
+     *
+     * @param integer $count
+     * @return Products
+     */
+    public function setCount($count)
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+
+    /**
+     * Get count
+     *
+     * @return integer 
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    /**
+     * Set feedbackId
+     *
+     * @param integer $feedbackId
+     * @return Products
+     */
+    public function setFeedbackId($feedbackId)
+    {
+        $this->feedbackId = $feedbackId;
+
+        return $this;
+    }
+
+    /**
+     * Get feedbackId
+     *
+     * @return integer 
+     */
+    public function getFeedbackId()
+    {
+        return $this->feedbackId;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
